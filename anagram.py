7ad809de5f3711aa166c045eaeba70c668c0b66f
zohaib10 <zohaib.m.ahmad@gmail.com>
2019-02-04 23:41:21 -0800

    adding completed anagram code

--- anagram.py
+++ anagram.py
@@ -1,1 +1,28 @@
-print("HI");
+# Question: Given two Strings write a function to determine if they are anagrams?
+# Run time O(n)
+
+
+def isAnagram(s1, s2):
+    s1 = s1.lower()
+    s2 = s2.lower()
+    dict = [0] * 1000
+
+    for c in s1:
+        dict[ord(c)] += 1
+
+    for c in s2:
+        dict[ord(c)] -= 1
+
+    for i in dict:
+        if i != 0:
+            return False
+
+    return True
+
+
+
+
+if(isAnagram("Listen", "Silent")):
+    print("Is Anagram")
+else:
+    print("Is not Anagram")
